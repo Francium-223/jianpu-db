b74b4739f723e482332826de48f47ab1a37b1c9f
Francium-223 <wizardofyendor@outlook.com>
2026-07-25 14:40:31 +0800

    update .gitignore: by_*/, data.json

--- parse_scores.py
+++ parse_scores.py
@@ -2,7 +2,12 @@ from score import *
 files = os.listdir('scores')
 files = ['scores/' + i for i in files if re.match(r'.*\.txt', i)]
 a = []
+b = {}
 for i in files:
 	a.append(Score(i))
-	print('Parsing:', i)
-	a[len(a) - 1].parse()
+for i in a:
+	print('Parsing:', i.score)
+	i.parse()
+	b.update({i.mbid : i.others})
+with open('./data_buf.json', 'w', encoding='utf-8') as f:
+	json.dump(b, f, indent=4, ensure_ascii=False)
